99e8095c26c0f5055e643adbb83ba666456a1e21
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-24 22:20:46 -0600

    initial repository of build folder

---
+++ precache-manifest.cf6be099992fe2428605f3a6ff2eb3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930d0a1b73076e906dc8288b0cb3c423",
+    "url": "/FinancialAnalysis/index.html"
+  },
+  {
+    "revision": "622cb1edf9db83a32517",
+    "url": "/FinancialAnalysis/static/css/main.be6c77d9.chunk.css"
+  },
+  {
+    "revision": "24541ecd081dd80666dd",
+    "url": "/FinancialAnalysis/static/js/2.45fe23f9.chunk.js"
+  },
+  {
+    "revision": "622cb1edf9db83a32517",
+    "url": "/FinancialAnalysis/static/js/main.585727eb.chunk.js"
+  },
+  {
+    "revision": "4a16025dd87859b6e0bc",
+    "url": "/FinancialAnalysis/static/js/runtime~main.cbc65847.js"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/FinancialAnalysis/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  }
+]);
